bb4fae36b892131fcc5eeae329b8867a840c3535
Allen <1solomonall@gmail.com>
2024-09-24 19:31:31 -0400

    Im just playing around

---
+++ counterApp/counterApp/counterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  counterAppApp.swift
+//  counterApp
+//
+//  Created by Alden Solomon on 9/24/24.
+//
+
+import SwiftUI
+
+@main
+struct counterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
